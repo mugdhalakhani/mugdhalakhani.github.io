3ba7f3f792d57bd131108862c07c7cfd55c81d8a
Mugdha Lakhani <nator@chromium.org>
2019-06-27 20:05:57 +0100

    Add style

--- periodic_sync/sw.js
+++ periodic_sync/sw.js
@@ -1,9 +1,5 @@
 console.log('Im loaded');
 
-self.addEventListener('fetch', event => {
-  event.respondWith(event.request, {cache: "no-cache"});
-});
-
 self.addEventListener('periodicsync', event => {
   console.log('Periodic Sync received for ' + event.tag);
 });
